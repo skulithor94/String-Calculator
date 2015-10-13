28055636e91b8c843d8b7e23eea7e74422f83c8c
Skuli <skuliarnas@gmail.com>
2015-10-13 11:12:39 +0000

    another newline test passed

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -39,4 +39,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
+    @Test 
+    public void testOnlyNewLine(){
+    	assertEquals(6, Calculator.add("1\n2\n3"));
+    }
+
 }
