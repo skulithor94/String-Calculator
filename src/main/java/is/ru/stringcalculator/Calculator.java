acd0fad0dbfb38b5bf03d07bfd21afa1e045bf1d
Skuli <skuliarnas@gmail.com>
2015-10-13 23:07:36 +0000

    Delimiter tests passes

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,12 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
+		if(text.startsWith("//")){
+			text = text.substring(2);
+			char temp = text.charAt(0);
+			text = text.replace(text.charAt(0), ',');
+			text = text = text.substring(2);
+		}
 		if(contansNewLine(text)){
 			text = replaceNewLine(text);
 		}
